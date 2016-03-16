95c4a79fe85413553c76e69c6fb8dbb354fd6ca0
zgibanez <zoiletum@gmail.com>
2016-03-16 13:40:46 +0100

    Update source.cpp
    arreglado error de cvtColor

--- source.cpp
+++ source.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
+#include <stdio.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv.hpp"
 
 using namespace cv;
 using namespace std;
+
+string type2str(int type) {
+	string r;
+
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+	switch (depth) {
+	case CV_8U:  r = "8U"; break;
+	case CV_8S:  r = "8S"; break;
+	case CV_16U: r = "16U"; break;
+	case CV_16S: r = "16S"; break;
+	case CV_32S: r = "32S"; break;
+	case CV_32F: r = "32F"; break;
+	case CV_64F: r = "64F"; break;
+	default:     r = "User"; break;
+	}
+
+	r += "C";
+	r += (chans + '0');
+
+	return r;
+}
+
 
 int main(int argc, char** argv)
 {
 	Mat img, img_r, img_g, img_b, imgHSV;
 	int low_h, high_h, low_s, high_s, low_v, high_v;
 
-	img = imread(argv[1], 0);
-	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+	img = imread(argv[1], 1);
+
+	string ty = type2str(img.type());
+	printf("Matrix: %s %dx%d \n", ty.c_str(), img.cols, img.rows);
+
+	
 
 	//Create window with trackbar
 	namedWindow("Control", CV_WINDOW_NORMAL);
@@ -29,6 +59,8 @@ int main(int argc, char** argv)
 		cout << "Error: image empty" << endl;
 		return 1;
 	}
+
+	cvtColor(img, imgHSV, COLOR_BGR2GRAY);
 
 	while (true)
 	{
